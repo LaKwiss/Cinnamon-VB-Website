6c06fbaef62ec67cab4abfcfebdd37f41cafae61
Yann Bälli <y.baelli05@gmail.com>
2023-09-07 15:31:55 +0200

    CSS CardStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 export default function App() {
     return (
         <>
-            <header><h1><a href="http://">Cinnamon</a></h1></header>
+            <header><h1><a href="https://">Cinnamon</a></h1></header>
             <Card title={"2242"} desc={"Kocher"}/>
             <Card title={"le leadership"} desc={"battlefield"}/>
             <Card title={"C++ tutourial"} desc={"made by testo numeor uno quentin pozner"}/>
@@ -16,11 +13,16 @@ export default function App() {
 
 function Card({title, desc}) {
     return (
-        <>
+        <div style={CardStyle}>
             <h1>{title}</h1>
             <p>{desc}</p>
-        </>
+        </div>
     )
 }
 
-
+const CardStyle = {
+    background: '#007AFF',
+    borderRadius: '55px',
+    padding: '30px',
+    marginTop: '30px'
+}
